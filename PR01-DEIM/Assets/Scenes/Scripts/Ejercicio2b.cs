52387e279f5b29fedb80bc72fb8277abae934b79
BlancaSJG <blancasanjose13@gmail.com>
2021-09-21 10:30:12 +0200

    Ejercicio 2b

---
+++ PR01-DEIM/Assets/Scenes/Scripts/Ejercicio2b.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2b : MonoBehaviour
+{
+
+    int n = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       for (int y = 0; y <= 15; y++)
+        {
+            print(y);
+        } 
+
+        while (n <= 15)
+         {
+            print(n);
+            n++;
+         }   
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        
+    }
+}
