d5fe7fd07c11b75da3228bb9841e513d816ce76c
BlancaSJG <blancasanjose13@gmail.com>
2021-09-21 09:33:02 +0200

    Ejercicio 1.a

--- PR01-DEIM/Assets/Scenes/Scripts/Ejercicio1a.cs
+++ PR01-DEIM/Assets/Scenes/Scripts/Ejercicio1a.cs
@@ -2,12 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Ejercicio1a : MonoBehaviour
 {
+    int n = 5;
+    float d = 10f;
+    bool despierto = false;
+    string texto = "Hola mundo";
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        print(n);
+        print(d);
+        print(despierto);
+        print(texto);
     }
 
     // Update is called once per frame
